a8387a8f01abca0ac04fb7e1c48204030e7de598
larper02 <53142287+larpedd@users.noreply.github.com>
2026-02-19 06:20:25 +0700

    Add URI_PROTOCOL constant to config

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
 pub const SETUP: &str = "https://setup.pekora.zip";
 pub const POST_INSTALL_URL: &str = "https://www.pekora.zip/download/thankyou";
 pub const YEARS: [&str; 4] = ["2017L", "2018L", "2020L", "2021M"]; //take nodes on things like these since im not THAT experienced with rust lol
+pub const URI_PROTOCOL: &str = "pekora-player://";
