3480fd014fa3132018aa95ee888db8849ae219ef
larper02 <53142287+larpedd@users.noreply.github.com>
2026-02-19 04:26:48 +0700

    Correct YEARS constant syntax in config.rs
    Fix syntax for YEARS constant declaration

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,3 @@
 pub const SETUP: &str = "https://setup.pekora.zip";
 pub const POST_INSTALL_URL: &str = "https://www.pekora.zip/download/thankyou";
-pub const YEARS: [&str: 4;] = ["2017L", "2018L", "2020L", "2021E"];
+pub const YEARS: [&str; 4] = ["2017L", "2018L", "2020L", "2021E"]; //take nodes on things like these since im not THAT experienced with rust lol
